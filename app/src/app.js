0a6a54d7c52a2d077ea3822c27a13d1dbf807f53
Enrique Cornejo <enrique@cornejo.me>
2019-08-06 13:20:23 +0200

    Hotfix -- don't deploy. New plugin version

--- app/src/app.js
+++ app/src/app.js
@@ -50,8 +50,8 @@ var port = process.env.PORT || config.get('service.port');
 
 
 server.listen(port, function () {    
-    const microserviceClient = require('vizz.microservice-client');
-    
+    //const microserviceClient = require('vizz.microservice-client');
+    const microserviceClient = require('ct-register-microservice-node');    
     microserviceClient.register({
         id: config.get('service.id'),
         name: config.get('service.name'),
@@ -60,12 +60,12 @@ server.listen(port, function () {
         logger: logger,
         app: app
     });
-    if (process.env.CT_REGISTER_MODE && process.env.CT_REGISTER_MODE === 'auto') {
-        microserviceClient.autoDiscovery(config.get('service.name')).then(() => {}, (err) => {
-            logger.error('Error registering');
-            process.exit(1);
-        });
-    }
+    // if (process.env.CT_REGISTER_MODE && process.env.CT_REGISTER_MODE === 'auto') {
+    //     microserviceClient.autoDiscovery(config.get('service.name')).then(() => {}, (err) => {
+    //         logger.error('Error registering');
+    //         process.exit(1);
+    //     });
+    // }
 });
 
 logger.info('Server started in port:' + port);
